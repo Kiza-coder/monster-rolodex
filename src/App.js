bf6d90e7325c6902f88a6dd42796f43100deced4
Kiza-coder <kiza.motoko@gmail.com>
2024-03-25 22:37:45 +0100

    Test Commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Kiza"
+    }
+  }
   render()
   {
     return (
@@ -10,16 +16,14 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome {this.state.name}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button 
+          onClick={() => {
+            this.setState({name: 'David'})
+          }}>
+            Change the name
+            </button>
         </header>
       </div>
     );
