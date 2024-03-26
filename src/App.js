0d3778092df3ac0c17e1151c6bb96ebf2886912f
Kiza-coder <kiza.motoko@gmail.com>
2024-03-26 21:01:16 +0100

    Lyfecycle

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Kiza"
+      monsters: []
     }
   }
-  render()
-  {
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      },
+        () => {
+          console.log(this.state);
+        }
+      ))
+  }
+
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome {this.state.name}
-          </p>
-          <button 
-          onClick={() => {
-            this.setState({name: 'David'})
-          }}>
-            Change the name
-            </button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1 key={monster.id}>{monster.name}</h1>
+          })
+        }
       </div>
     );
   }
